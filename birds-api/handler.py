ad045d5a7f182c49af4502401c8323914b201ff0
kdgromova <kdgromova@gmail.com>
2022-01-15 13:19:55 -0500

    added get_birds_videos and get_video

--- birds-api/handler.py
+++ birds-api/handler.py
@@ -10,7 +10,6 @@ def get_birds(event, context):
         KeyConditionExpression=Key('SK').eq('stats')
     )
     items = response['Items']
-    print(items)
 
     body = {
         "birds": items,
@@ -20,3 +19,32 @@ def get_birds(event, context):
 
     return response
 
+def get_bird_videos(event, context):
+    species = event['pathParameters']['species'].upper()
+    response = table.query(
+        KeyConditionExpression=Key('PK').eq('species#'+species)
+    )
+    items = response['Items']
+    
+    body = {
+        "videos": items,
+    }
+    
+    response = {"statusCode": 200, "body": json.dumps(body)}
+    return response
+
+def get_video(event, context):
+    # Example: 2022_01_01_20-12-11
+    video = event['pathParameters']['video'].replace("_", "/")
+    response = table.query(
+        IndexName='SK-PK',
+        KeyConditionExpression=Key('SK').eq('video#'+video)
+    )
+    items = response['Items']
+    
+    body = {
+        "birds": items,
+    }
+    
+    response = {"statusCode": 200, "body": json.dumps(body)}
+    return response
